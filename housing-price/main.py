96bacc32d93c9ef129d630c8fef98759e267f235
Elahe Dastan <elahe.dstn@gmail.com>
2020-09-04 23:57:07 +0430

    preprocess the data

--- housing-price/main.py
+++ housing-price/main.py
@@ -22,6 +22,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestRegressor
 import joblib
 from sklearn.model_selection import GridSearchCV
+from scipy import stats
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -237,15 +238,48 @@ forest_reg = RandomForestRegressor()
 # joblib.dump(forest_reg, "forest_model")
 # my_model = joblib.load("forest_model")
 
+# param_grid = [
+#     {'n_estimators': [3, 10, 30], 'max_features':[2, 4, 6, 8]},
+    # {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+# ]
 param_grid = [
-    {'n_estimators': [3, 10, 30], 'max_features':[2, 4, 6, 8]},
-    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+    {'n_estimators': [30], 'max_features':[8]},
 ]
 
 grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error', return_train_score=True)
 grid_search.fit(housing_prepared, housing_labels)
-print(grid_search.best_params_)
-print(grid_search.best_estimator_)
-cvres = grid_search.cv_results_
-for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
-    print(np.sqrt(-mean_score), params)
+# print(grid_search.best_params_)
+# print(grid_search.best_estimator_)
+# cvres = grid_search.cv_results_
+# for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+#     print(np.sqrt(-mean_score), params)
+
+# feature_importance = grid_search.best_estimator_.feature_importances_
+# print(feature_importance)
+#
+# extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+# cat_encoder = full_pipeline.named_transformers_["cat"]
+# cat_one_hot_attribs = list(cat_encoder.categories_[0])
+# attributes = num_attribs + extra_attribs + cat_one_hot_attribs
+# print(sorted(zip(feature_importance, attributes), reverse=True))
+
+final_model = grid_search.best_estimator_
+
+X_test = strat_test_set.drop("median_house_value", axis=1)
+y_test = strat_test_set["median_house_value"].copy()
+
+X_test_prepared = full_pipeline.transform(X_test)
+
+final_predictions = final_model.predict(X_test_prepared)
+# for y, fp in zip(y_test, final_predictions):
+#     print(y, fp)
+
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
+print(final_rmse)
+
+# confidence = 0.95
+# squared_errors = (final_predictions - y_test) ** 2
+# print(np.sqrt(stats.t.interval(confidence, len(squared_errors) - 1, loc=squared_errors.mean(),
+#                                scale=stats.sem(squared_errors))))
+
