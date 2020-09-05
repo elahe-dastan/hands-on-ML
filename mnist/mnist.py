ff56bfefc90eac5f5b6905ee5aec1dfdf97bf9be
Elahe Dastan <elahe.dstn@gmail.com>
2020-09-05 22:56:33 +0430

    page 95

---
+++ mnist/mnist.py
@@ -0,0 +1,90 @@
+from sklearn.datasets import fetch_openml
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+from sklearn.model_selection import cross_val_score
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_curve
+
+
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
+
+# The last precision and recall values are 1. and 0. respectively and do not
+# have a corresponding threshold.
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+
+
+mnist = fetch_openml('mnist_784', version=1)
+X, y = mnist["data"], mnist["target"]
+y = y.astype(np.uint8)
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+sgd_clf = SGDClassifier(random_state=42)
+# sgd_clf.fit(X_train, y_train_5)
+
+# skfolds = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
+#
+# for train_index, test_index in skfolds.split(X_train, y_train_5):
+#     clone_clf = clone(sgd_clf)
+#     X_train_folds = X_train[train_index]
+#     y_train_folds = y_train_5[train_index]
+#     X_test_fold = X_train[test_index]
+#     y_test_fold = y_train_5[test_index]
+#
+#     clone_clf.fit(X_train_folds, y_train_folds)
+#     y_pred = clone_clf.predict(X_test_fold)
+#     n_correct = sum(y_pred == y_test_fold)
+#     print(n_correct / len(y_pred))
+
+# never_5_clf = Never5Classifier()
+# print(cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+
+# y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+
+# print(confusion_matrix(y_train_5, y_train_pred))
+# print(precision_score(y_train_5, y_train_pred))
+# print(recall_score(y_train_5, y_train_pred))
+# print(f1_score(y_train_5, y_train_pred))
+
+
+# some_digit = X[0]
+# some_digit_image = some_digit.reshape(28, 28)
+#
+# plt.imshow(some_digit_image, cmap=mpl.cm.binary, interpolation="nearest")
+# plt.axis("off")
+# plt.show()
+
+# y_score = sgd_clf.decision_function([some_digit])
+# print(y_score)
+# threshold = 0
+# y_some_digit_pred = (y_score > threshold)
+# print(y_some_digit_pred)
+# threshold = 8000
+# y_some_digit_pred = (y_score > threshold)
+# print(y_some_digit_pred)
+
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+# print(precisions)
+# print(recalls)
+# print(thresholds)
+
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.show()
