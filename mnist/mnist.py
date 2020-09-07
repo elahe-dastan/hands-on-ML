c8b9264b812e9228412aa31b9ea9df7f3bf8183b
Elahe Dastan <elahe.dstn@gmail.com>
2020-09-07 17:50:41 +0430

    multiple classifier

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -9,7 +9,10 @@ from sklearn.model_selection import cross_val_score
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_curve, roc_curve
+from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_curve, roc_curve, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.preprocessing import StandardScaler
 
 
 class Never5Classifier(BaseEstimator):
@@ -41,6 +44,8 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 y_train_5 = (y_train == 5)
 y_test_5 = (y_test == 5)
 
+print("got data")
+
 sgd_clf = SGDClassifier(random_state=42)
 # sgd_clf.fit(X_train, y_train_5)
 
@@ -69,7 +74,9 @@ sgd_clf = SGDClassifier(random_state=42)
 # print(f1_score(y_train_5, y_train_pred))
 
 
-# some_digit = X[0]
+some_digit = X[0]
+# print(some_digit.astype(np.float64))
+# print(y_train[0])
 # some_digit_image = some_digit.reshape(28, 28)
 #
 # plt.imshow(some_digit_image, cmap=mpl.cm.binary, interpolation="nearest")
@@ -85,8 +92,8 @@ sgd_clf = SGDClassifier(random_state=42)
 # y_some_digit_pred = (y_score > threshold)
 # print(y_some_digit_pred)
 
-y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
-precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+# y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
+# precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
 # print(precisions)
 # print(recalls)
 # print(thresholds)
@@ -100,7 +107,50 @@ precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
 # print(precision_score(y_train_5, y_train_pred_90))
 # print(recall_score(y_train_5, y_train_pred_90))
 
-fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+# fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+#
+# plot_roc_curve(fpr, tpr)
+# plt.show()
 
-plot_roc_curve(fpr, tpr)
+# print(roc_auc_score(y_train_5, y_scores))
+
+forest_clf = RandomForestClassifier(random_state=42)
+# y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
+# y_scores_forest = y_probas_forest[:, 1] # score = proba of positive class
+# fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
+#
+# plt.plot(fpr, tpr, "b:", label="SGD")
+# plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
+# plt.show()
+
+# print(roc_auc_score(y_train_5, y_scores_forest))
+
+# sgd_clf.fit(X_train, y_train)
+# print(sgd_clf.predict([some_digit]))
+# some_digit_scores = sgd_clf.decision_function([some_digit])
+# print(some_digit_scores)
+#
+# print(np.argmax(some_digit_scores))
+# print(sgd_clf.classes_)
+
+# ovo_clf = OneVsOneClassifier(SGDClassifier(random_state=42))
+# ovo_clf.fit(X_train, y_train)
+# print(ovo_clf.predict([some_digit]))
+# print(len(ovo_clf.estimators_))
+
+# forest_clf.fit(X_train, y_train)
+# print(forest_clf.predict([some_digit]))
+# print(forest_clf.predict_proba([some_digit]))
+
+# print(cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring="accuracy"))
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+print("scaled")
+print(cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring="accuracy"))
+
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+conf_mx = confusion_matrix(y_train, y_train_pred)
+print(conf_mx)
+plt.matshow(conf_mx, cmap=plt.cm.gray)
 plt.show()
