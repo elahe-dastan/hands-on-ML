e7be920a61353ebc03a3bfc5843d666584ca03ba
elahe dastan <elahe.dstn@gmail.com>
2020-10-24 11:07:56 +0330

    using PCA

--- dimensionality_reduction/dimensionality_reduction.py
+++ dimensionality_reduction/dimensionality_reduction.py
@@ -1,14 +1,17 @@
 import numpy as np
+from sklearn.decomposition import PCA
 
 X = np.array([[3, 2, 2],
               [2, 3, -2]])
 
-X_centered = X - X.mean(axis=0)
-U, s, Vt = np.linalg.svd(X_centered)
-c1 = Vt.T[:, 0]
-c2 = Vt.T[:, 1]
+# X_centered = X - X.mean(axis=0)
+# U, s, Vt = np.linalg.svd(X_centered)
+# c1 = Vt.T[:, 0]
+# c2 = Vt.T[:, 1]
+#
+# W2 = Vt.T[:, :2]
+# X2D = X_centered.dot(W2)
 
-W2 = Vt.T[:, :2]
-X2D = X_centered.dot(W2)
-
-
+pca = PCA(n_components=2)
+X2D = pca.fit_transform(X)
+print(pca.components_)
